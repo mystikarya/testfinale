50947323aff7c07440575a7ac9a9b197b95f5949
mystikarya <mystikaryainfo@gmail.com>
2026-07-21 14:41:45 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -246,7 +246,7 @@ async function generateCertificate() {
         canvas.height = image.height;
 
         ctx.drawImage(image, 0, 0);
-        let size = 520;
+        let size = 400;
         ctx.font = `${size}px Elven`;
         while (
             ctx.measureText(name).width > 900 &&
@@ -265,7 +265,7 @@ async function generateCertificate() {
         ctx.fillText(
             name,
             canvas.width / 2,
-            860
+            600
         );
 
         const link = document.createElement("a");
